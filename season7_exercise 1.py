bf46fc007086ce7123f75af20291b122be25d862
mohamad salmasi <mdsalmac@gmail.com>
2022-03-14 18:49:47 +0330

    last part

--- season7_exercise 1.py
+++ season7_exercise 1.py
@@ -8,11 +8,13 @@ def get_gheymat_float(gheymat):
 
 def informize(gheymat):
     api_key="376F337149506F644E6D72314A6E4E59456652423530765851744757702B2F46386F6D324A4562373346303D"
-    url="https://api.kavenegar.com/v1/%S/sms/send.json" %api_key
-    pay_load={'receptor':'09013219653','message':' price is really low baby common buy it. it is only %i$'%gheymat}
-    requests.post(url,data=pay_load)
+    url="https://api.kavenegar.com/v1/%s/sms/send.json" %api_key
+    pay_load={'receptor':'+989013219653','message':'price is %s$'%gheymat}
+    respons=requests.post(url,pay_load)
+    print(respons)
 import requests
 
 r=requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
 gheymat=(r.json()['bpi']['USD']['rate'])
-print()
+informize(gheymat)
+# print(gheymat)
