7ba5dcd3c168c24173354627d52aba16be7fe4c5
mohamad salmasi <mdsalmac@gmail.com>
2022-03-14 17:59:32 +0330

    add a server to send a sms

--- season7_exercise 1.py
+++ season7_exercise 1.py
@@ -5,8 +5,14 @@ def get_gheymat_float(gheymat):
     else:
         gheymat_float=gheymat[:x]+gheymat[x+1:]
         return float(gheymat_float)
+
+def informize(gheymat):
+    api_key="376F337149506F644E6D72314A6E4E59456652423530765851744757702B2F46386F6D324A4562373346303D"
+    url="https://api.kavenegar.com/v1/%S/sms/send.json" %api_key
+    pay_load={'receptor':'09013219653','message':' price is really low baby common buy it. it is only %i$'%gheymat}
+    requests.post(url,data=pay_load)
 import requests
 
 r=requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
 gheymat=(r.json()['bpi']['USD']['rate'])
-print(get_gheymat_float(gheymat))
+print()
